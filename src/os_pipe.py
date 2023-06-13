a1f5dcf954752d085e4fe8ee6df9bdeeca665669
unknown <ronnielow@protonmail.com>
2023-06-13 21:02:19 +0800

    fix compatibility issue

--- src/os_pipe.py
+++ src/os_pipe.py
@@ -10,7 +10,9 @@ def play(path):
     # pipe into playsound whcih opens a new python instance that doesn't hold up the stack
     # finds local path absolute to its location through os
     localpath = f"{os.path.abspath(os.getcwd())}\src\playsound.py"
+    path = f"{os.path.abspath(os.getcwd())}\{path}"
 
     # opens a new process separate from current running process and automatically closes itself after playing sound
     # localpath represents file executable to playsound.py and sys.executable runs it as a python file
     subprocess.Popen([sys.executable, localpath, path], stderr=subprocess.PIPE)
+    print(path)
